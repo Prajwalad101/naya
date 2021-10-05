cb15fba4b134525f57d33b2cb3ca37336462f9ea
Prajwal Adhikari <prajwalad101@gmail.com>
2021-10-05 10:38:23 +0545

    modify layout of item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,19 @@
-import { FaCartPlus } from 'react-icons/fa';
+import { BsCartPlus } from 'react-icons/bs';
 
 export const Item = ({ name, price, image }) => {
   return (
-    <div>
-      <img src={image} alt='name' />
-      <h5>{name}</h5>
-      <p>Rs.{price}</p>
-      <p>Add to cart</p>
-      <FaCartPlus size='1.5rem' />
+    <div className='item'>
+      <img src={image} alt='name' className='item-children-top' />
+      <div className='item-children-bottom'>
+        <div className='item-children-left'>
+          <h5>{name}</h5>
+          <p>Rs.{price}</p>
+        </div>
+        <div className='item-children-right'>
+          <p>Add to cart</p>
+          <BsCartPlus size='1.5rem' />
+        </div>
+      </div>
     </div>
   );
 };
